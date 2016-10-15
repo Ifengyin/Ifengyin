95e37954a06b60f046c7480d47961baf1270c57e
Ifengyin <18397642848@qq.com>
2016-10-15 10:12:23 +0800

    myboot01

---
+++ myboot/js/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2016/10/10/010.
+ */
+// function CalcMeasure(){
+// 	var wH=$(window).height();
+// 	var wW=$(window).width();
+// 	$('.section-container>section').attr("style","min-height:"+wH+'px;height:'
+// 	+wH+"px;");
+// 	$('#intro').attr("style","min-height:"+wH+"px;height:"+wH+"px;");
+// 	if(wW<767){
+// 		wH=wH-100;
+// 		wW=wW-100;
+// 		$("#intro").attr("style","min-height:"+wH+"px;height:"+wH+"px;");
+// 	}
+// }
+// CalcMeasure();
